dd465ec73d17a71d75ef917182caa4ad82650220
JasonJasonJason <jasonharris1991@gmail.com>
2013-11-06 02:21:28 -0600

    Deleted the prematurely merged branch

--- fanhype/config_example.py
+++
@@ -1,9 +0,0 @@
-class Config(object):
-    def __init__(self):
-        self.consumer_key = 
-        self.consumer_secret = 
-        self.access_token_key = 
-        self.access_token_secret = 
-
-    def get_keys(self):
-    	return [self.consumer_key, self.consumer_secret, self.access_token_key, self.access_token_secret]
