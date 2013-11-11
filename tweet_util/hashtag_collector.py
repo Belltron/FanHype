f72df37c72dc53f7ab253d63305e205b51b44188
William Guerra <williamtguerra@gmail.com>
2013-11-11 15:44:56 -0600

    got things from master

---
+++ tweet_util/hashtag_collector.py
@@ -0,0 +1,59 @@
+import tweepy
+import json
+
+
+class TweetCollector():
+    def __init__(self):
+    	self.consumer_key = ''
+	self.consumer_secret = ''
+	self.access_token_key = ''
+	self.access_token_secret = ''
+
+    def CollectTweets(self, query):
+        auth = tweepy.OAuthHandler(self.consumer_key, self.consumer_secret)
+        auth.set_access_token(self.access_token_key, self.access_token_secret)
+        api = tweepy.API(auth)
+        
+        tw = {}
+        
+        tweetList = []
+        hashTagText = ""
+        hashTags = []
+        tagInfo = []
+        location = ""
+        tagTuples = []
+        for tweet in tweepy.Cursor(api.search,
+            q=query,
+            rpp=100,
+            result_type="recent",
+            include_entities=True,
+            lang="en").items(500):
+            tw['entities'] = tweet.entities            
+            tw['user'] = tweet.user
+            tagInfo = ((tw['entities'])['hashtags'])
+            for tag in tagInfo:
+                hashTags.append(tag['text'])
+        #print hashTags
+
+        for tag in hashTags:
+            count = hashTags.count(tag)
+            tuple = (count,tag)
+            if tuple not in tagTuples:
+                tagTuples.append(tuple)
+
+        tagTuples = sorted(tagTuples, key = lambda tup: -tup[0])
+        for tup in tagTuples:
+            print tup[1], '\t', tup[0]
+        
+            
+        """
+        #Print to file for testing purposes
+        fileName = query + ".json"
+        with open(fileName, 'w') as outfile:
+            json.dump(tagTuples, outfile, indent = 0)
+        """
+        
+if __name__ == "__main__":
+    query = raw_input('Enter a Twitter query: ')
+    collector = TweetCollector()
+    collector.CollectTweets(query,)
---
+++ tweet_util/tweet_shrinker.py
@@ -0,0 +1,56 @@
+import os
+import json
+import sys
+
+
+if len(sys.argv) != 3:
+	print sys.argv[0] + " <input_file_name> <output_file_name>"
+	sys.exit(2)
+input_file_name = sys.argv[1]
+output_file_name = sys.argv[2]
+
+output_file = open(output_file_name, "a")
+
+
+def read_data(filename):
+	data = []
+	try:
+		with open(filename) as f:
+			for line in f:
+				data.append (json.loads (line.strip()))
+	except:
+		print "Error reading file: "+filename
+		return []
+	return data
+
+
+def get_tweets():
+    return read_data(os.path.join(os.getcwd(), input_file_name))
+
+
+def transform_tweets(tweets):
+	for tweet in tweets:
+		short_tweet = {
+			"coordinates": tweet['coordinates'],
+			"created_at": tweet["created_at"],
+			"entities": { 
+				'hashtags': tweet["entities"]["hashtags"]
+				},
+			"favorite_count": tweet["favorite_count"],
+			"filter_level": tweet["filter_level"],
+			"id": tweet["id"],
+			"lang": tweet["lang"],
+			"retweet_count": tweet["retweet_count"],
+			"text": tweet["text"],
+			 "user": {
+			 	"screen_name": tweet['user']['screen_name'],
+			 	"profile_img_url": tweet['user']['profile_image_url'],
+			 	"verified": tweet['user']['verified'],
+			 	'followers_count': tweet['user']['followers_count'],
+			 	'friends_count': tweet['user']['friends_count']
+			 	}}
+		output_file.write (json.dumps (short_tweet) + '\n')
+
+
+tweets = get_tweets()
+transform_tweets(tweets)
