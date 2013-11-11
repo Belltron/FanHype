88bac165a89a1f6bf36f3fc6feb402a738eacc36
JasonJasonJason <jasonharris1991@gmail.com>
2013-11-10 19:07:15 -0600

    Changed the tweet_saver to save tweets in json format.

--- fanhype/tweet_saver.py
+++ fanhype/tweet_saver.py
@@ -12,22 +12,19 @@ class StdOutListener(StreamListener):
 		x = 1
 
 	def on_data(self, data):
-		tweet = json.loads(data)
-		print tweet
+		sys.stdout.write(data)
+		sys.stdout.flush()
 		return True
 
 	def on_error(self, status):
 		print status
 
-c = config.Config()
-keys = c.get_keys()
-
+keys = config.Config().get_keys()
 auth = OAuthHandler(keys[0], keys[1])
 auth.set_access_token(keys[2], keys[3])
 
-l = StdOutListener()
+stream = Stream(auth, StdOutListener())
 
-stream = Stream(auth, l)
 # hashtags by game
 stanford_oregon = ['#gostanford', '#beatou', '#beatoregon', '#stanford',  '#ou', '#oregonfootball', '#ducksfootball', '#goducks', '#beatstanford']
 baylor_oklahoma = ['#Baylor', '#SicEm', '#SicEmBears', '#WatchBU', '#EveryoneInBlack', '#SicOU', '#Sooners', '#BeatBaylor', '#BoomerSooner', '#gosooners']	
