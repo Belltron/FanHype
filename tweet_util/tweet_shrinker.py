f8b4ee5ce405da1cc60f61f2cd2f5bb5eed63030
William Guerra <williamtguerra@gmail.com>
2013-12-09 17:25:22 -0600

    donezo

--- tweet_util/tweet_shrinker.py
+++ tweet_util/tweet_shrinker.py
@@ -31,26 +31,29 @@ def get_tweets():
 
 def transform_tweets(tweets):
 	for tweet in tweets:
-		short_tweet = {
-			"coordinates": tweet['coordinates'],
-			"created_at": tweet["created_at"],
-			"entities": { 
-				'hashtags': tweet["entities"]["hashtags"]
-				},
-			"favorite_count": tweet["favorite_count"],
-			"filter_level": tweet["filter_level"],
-			"id": tweet["id"],
-			"lang": tweet["lang"],
-			"retweet_count": tweet["retweet_count"],
-			"text": tweet["text"],
-			 "user": {
-			 	"screen_name": tweet['user']['screen_name'],
-			 	"profile_img_url": tweet['user']['profile_image_url'],
-			 	"verified": tweet['user']['verified'],
-			 	'followers_count': tweet['user']['followers_count'],
-			 	'friends_count': tweet['user']['friends_count']
-			 	}}
-		output_file.write (json.dumps (short_tweet) + '\n')
+		try:
+			short_tweet = {
+				"coordinates": tweet['coordinates'],
+				"created_at": tweet["created_at"],
+				"entities": { 
+					'hashtags': tweet["entities"]["hashtags"]
+					},
+				"favorite_count": tweet["favorite_count"],
+				"filter_level": tweet["filter_level"],
+				"id": tweet["id"],
+				"lang": tweet["lang"],
+				"retweet_count": tweet["retweet_count"],
+				"text": tweet["text"],
+				 "user": {
+				 	"screen_name": tweet['user']['screen_name'],
+				 	"profile_img_url": tweet['user']['profile_image_url'],
+				 	"verified": tweet['user']['verified'],
+				 	'followers_count': tweet['user']['followers_count'],
+				 	'friends_count': tweet['user']['friends_count']
+				 	}}
+			output_file.write (json.dumps (short_tweet) + '\n')
+		except:
+			print 'oops'
 
 
 tweets = get_tweets()
